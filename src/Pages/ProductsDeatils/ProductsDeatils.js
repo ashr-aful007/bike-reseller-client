11328ab05db9bcfdb145b419d0dc2bf7f9ba34e2
ashr-aful007 <ashraful99980@gmail.com>
2022-11-26 11:37:41 +0600

    catagory card add

--- src/Pages/ProductsDeatils/ProductsDeatils.js
+++ src/Pages/ProductsDeatils/ProductsDeatils.js
@@ -7,7 +7,11 @@ function ProductsDeatils() {
     
      
   return (
-    <div>
+      <div className='m-3'>
+        <div>
+            <h1>chose your best one</h1>
+        </div>
+        <div className='grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5'>
         {
           products.map(product => <ShowProductsDeatils 
             key={product._id}
@@ -15,6 +19,7 @@ function ProductsDeatils() {
           ></ShowProductsDeatils>)
         }
     </div>
+      </div>
   )
 }
 
